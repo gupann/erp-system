b1ce151b8224a86a0a109d6f695b0aee82ba33d0
Anmol Gupta <anmolgupta23@ucla.edu>
2025-07-05 17:41:17 -0700

    Created dashboard page layout with sidebar, navbar, dashboard area

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,7 @@
+import Dashboard from  "@/app/dashboard/page";
+
 export default function Home() {
   return (
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        hello
-      </main>
+    <Dashboard/>
   );
 }
