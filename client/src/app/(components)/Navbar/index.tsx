257cac4fbf6d2efe7aaa8371b42c8d2b3ca79587
Anmol Gupta <anmolgupta23@ucla.edu>
2025-07-07 17:49:25 -0700

    Polished sidebar and navbar

--- client/src/app/(components)/Navbar/index.tsx
+++ client/src/app/(components)/Navbar/index.tsx
@@ -3,32 +3,42 @@
 import React from 'react'
 import { Menu, Moon, SearchIcon, Settings} from 'lucide-react'
 import Link from 'next/link'
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsSidebarCollapsed } from '@/state'
+import Image from 'next/image'
 
 const Navbar = () => {
-  return (
+
+    const dispatch = useAppDispatch();
+    const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+    );  
+
+    const switchSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+    };
+
+    return (
     <div className='flex justify-between items-center w-full mb-7'>
-        
-        {/* left side */}
         <div className='flex justify-between items-center gap-5'>
             <button 
                 className='px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' 
-                onClick={() => {}}>
-                <Menu className='w-4 h-4' />
+                onClick={switchSidebar}>
+                <Menu className='w-6 h-6' />
             </button>
             <div className='relative'>
                 <input 
                     type='search' 
-                    placeholder='Search for inventory, stockpiles and transactions...' 
-                    className='pl-10 pr-4 py-2 w-50 md:w-80 border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500'/>
+                    placeholder='Search inventory, stockpiles, or POs…'
+                    className="pl-10 pr-4 py-2 w-48 md:w-80 lg:w-[30rem] border-2 border-gray-300 bg-white rounded-lg focus:outline-none focus:border-blue-500" />
                 <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
                     <SearchIcon className='text-gray-500' size={20}/>
                 </div>
             </div>
         </div>
 
-        {/* right side */}
         <div className='flex justify-between items-center gap-5'>
-            <div className='hidden md:flex justify-between items-center gap-5'>
+            <div className='hidden md:flex justify-between items-center gap-5 pl-4'>
                 <div>
                     {/* TO DO: write dark mode logic and integrate */}
                     <button onClick={() => {}}>
@@ -37,8 +47,14 @@ const Navbar = () => {
                 </div>
                 <hr className='w-0 h-7 border border-solid border-l border-gray-300 mx-3'/>
                 <div className='flex items-center gap-3 cursor-pointer'>
-                    <div className='w-9 h-9'>pfp</div>
-                    <span className='font-semibold'>Scrapyard Name</span>
+                    <Image
+                        src="/scrapyard-pfp.png"
+                        alt="Scrapyard profile pic"
+                        width={27}
+                        height={27}
+                        className="rounded"
+                    />
+                    <span className='font-semibold'>Krusty Scrap</span>
                 </div>
             </div>
             {/* TO DO: create settings page and link */}
@@ -47,7 +63,7 @@ const Navbar = () => {
             </Link>
         </div>
     </div>
-  )
+    )
 }
 
 export default Navbar
--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,51 +1,83 @@
 "use client"
 
-import { Layout, LucideIcon, Menu } from 'lucide-react'
+import { Layout, LucideIcon, Clipboard, Menu, Truck, SlidersHorizontal, CircleDollarSign } from 'lucide-react'
 import { usePathname } from "next/navigation";
 import Link from 'next/link';
 import React from 'react'
-
+import { useAppDispatch, useAppSelector } from '@/app/redux';
+import { setIsSidebarCollapsed } from '@/state';
+import Image from 'next/image';
 interface SidebarLinkProps {
     href: string;
     icon: LucideIcon;
     label: string;
+    isCollapsed: boolean;
 }
 
 const SidebarLink = ({
-    href,
-    icon: Icon,
-    label,
+  href,
+  icon: Icon,
+  label,
+  isCollapsed,
 }: SidebarLinkProps) => {
-    const pathname = usePathname();
-    const isActive = pathname === href || (pathname === "/" && href === "/dashboard");
+  const pathname = usePathname();
+  const isActive =
+    pathname === href || (pathname === "/" && href === "/dashboard");
 
-    return (
-        <Link href={href}>
-            <div className={`cursor-pointer flex items-center hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors 
-                ${isActive ? "bg-blue-200 text-white" : ""}}`}>
-                <Icon className="w-6 h-6 !text-gray-700" />
-                <span className='font-medium text-gray-700'> {label} </span>
-            </div>
-        </Link>
-    )
-}
-const Sidebar = () => {
   return (
-    <div>
-        <div className='flex gap-3 justify-between md:justify-normal items-center pt-8'>
-            <div>logo</div>
-            <h1 className='font-extrabold text-2xl'>Optica</h1>
+    <Link href={href}>
+      <div
+        className={`cursor-pointer flex items-center text-xl hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors rounded-2xl
+          ${isCollapsed ? "justify-center py-4" : "justify-start px-8 py-4"}
+         ${isActive ? "bg-blue-200 text-white" : ""}
+      }`}
+      >
+        <Icon className="w-8 h-8 !text-gray-700" />
+
+        <span className={`${isCollapsed ? "hidden" : "block"} font-medium text-gray-700`}>
+          {label}
+        </span>
+      </div>
+    </Link>
+  );
+};
+
+const Sidebar = () => {
+
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );  
+
+  const switchSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+  };
+
+  const sidebarView = `fixed flex flex-col ${isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"} bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`
+  
+  return (
+    <div className={sidebarView}>
+        <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${isSidebarCollapsed ? "px-5" : "px-8"}`}>
+            <Image
+              src="/optica-logo.png"
+              alt="Optica logo"
+              width={27}
+              height={27}
+              className="rounded w-8"
+            />
+            <h1 className={`font-extrabold text-2xl ${isSidebarCollapsed ? "hidden" : "block"}`}>Optica</h1>
             <button 
-                className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={() => {}}>
-                <Menu className='w-4 h-4'/>
+                className='md:hidden px-3 py-3 bg-gray-100 rounded-full hover:bg-blue-100' onClick={switchSidebar}>
+                <Menu className='w-6 h-6'/>
             </button>
         </div>
 
-        <div className='flex-grow mt-8'>
-            <SidebarLink href="/dashboard" icon={Layout} label='Dashboard'/>
-            <SidebarLink href="/tracking" icon={Layout} label='Tracking'/>
-            <SidebarLink href="/inventory" icon={Layout} label='Inventory'/>
-            <SidebarLink href="/transaction" icon={Layout} label='Transactions'/>
+        <div className='flex-grow mt-8 flex flex-col gap-y-4'>
+            <SidebarLink href="/dashboard" icon={Layout} label='Dashboard' isCollapsed={isSidebarCollapsed}/>
+            <SidebarLink href="/track" icon={Truck} label='Track Inventory' isCollapsed={isSidebarCollapsed} />
+            <SidebarLink href="/manage" icon={Clipboard} label='Manage Stock'isCollapsed={isSidebarCollapsed} />
+            <SidebarLink href="/transactions" icon={CircleDollarSign} label='Transactions' isCollapsed={isSidebarCollapsed} />
+            <SidebarLink href="/settings" icon={SlidersHorizontal} label='Settings' isCollapsed={isSidebarCollapsed} />
         </div>
     </div>
   )
